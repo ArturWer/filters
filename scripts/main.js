c98ccafe1b2ccb69b8d26bfc53f5611c047ffcf1
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-14 13:15:47 +0200

    Added array in js file and init some variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
 "use strict"
+let persons = [
+	{name:"John", rate: 50},
+	{name:"Anna", rate: 115},
+	{name:"Frieda", rate: 20},
+	{name:"Lucielle", rate: 80},
+	{name:"Jan", rate: 70},
+	{name:"Bob", rate: 25},
+	{name:"Isabella", rate: 60},
+	{name:"Mary", rate: 75},
+];
+let table = document.querySelector('table');
+let max = 85;
+let min = 30;
+console.table(persons);
+
 	
